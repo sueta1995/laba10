766d90003dd2b458fe4f9bad5c289820af90a3f3
sueta1995 <tagir.kitaev@inbox.ru>
2022-12-04 00:08:08 +0300

    rubocop

--- app/helpers/calculates_helper.rb
+++ app/helpers/calculates_helper.rb
@@ -1,9 +1,12 @@
-module CalculatesHelper
-	def set_numbers
-		@numbers = params[:numbers].split(' ')
-	end
+# frozen_string_literal: true
 
-	def valid?(array)
+# helper for calculates controller
+module CalculatesHelper
+  def set_numbers
+    @numbers = params[:numbers].split(' ')
+  end
+
+  def valid?(array)
     result = true if array.any?
 
     array.each { |x| result = false if x.scan(/[-0-9]/).length != x.length }
@@ -28,20 +31,20 @@ module CalculatesHelper
     result
   end
 
-	def assignment
-		@result = calculating(@numbers.collect(&:to_i))
+  def assignment
+    @result = calculating(@numbers.collect(&:to_i))
     @max_result = @result.max_by(&:length)
-		@all_data = { 'result': @result, 'max_result': @max_result }
-	end
+    @all_data = { 'result': @result, 'max_result': @max_result }
+  end
 
-	def create_file
+  def create_file
     data = @all_data.to_xml.split("\n")
     data.delete_at(0)
 
     header_version = ["<?xml version='1.0' encoding='UTF-8'?>"]
-		header_style = ["<?xml-stylesheet type='text/xsl' href='output.xslt'?>"]
+    header_style = ["<?xml-stylesheet type='text/xsl' href='output.xslt'?>"]
     output = header_version + header_style + data
 
-		File.open('public/output.xml', 'w') { |f| f.puts(output.join("\n")) } if defined? @all_data
-	end
+    File.open('public/output.xml', 'w') { |f| f.puts(output.join("\n")) } if defined? @all_data
+  end
 end
