067ccdf03cc54ca781e57c181c722e459b6c4060
sueta1995 <tagir.kitaev@inbox.ru>
2022-12-04 20:59:05 +0300

    edited tests

--- spec/requests/calculates_spec.rb
+++ spec/requests/calculates_spec.rb
@@ -15,5 +15,22 @@ RSpec.describe CalculatesController, type: :controller do
       get 'create', params: { numbers: '1 2 3 4', format: :xml }
       expect(response.content_type).to eq('application/xml; charset=utf-8')
     end
+
+    it 'should be html' do
+      get 'create', params: { numbers: '1 0', format: :html }
+      expect(response.content_type).to eq('text/html; charset=utf-8')
+    end
+  end
+
+  describe 'various input' do
+		it 'returns first output' do
+			get 'create', params: { numbers: '1 2 3 2 1 6 5' }
+			expect(assigns(:all_data)[:max_result]).to eq [1, 2, 3]
+    end
+
+    it 'returns second output' do
+      get 'create', params: { numbers: '1 5 -1 45 1 9 18 1' }
+			expect(assigns(:all_data)[:max_result]).to eq [1, 9, 18]
+    end
   end
 end
